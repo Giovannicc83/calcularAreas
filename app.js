e463504d617fa40a77c25ad523cd3041cf682a40
Giovanni <geovannycc@gmail.com>
2021-03-01 19:20:43 -0600

    Se crean los archivos app.js e index.html para el calculo de áreas

---
+++ app.js
@@ -0,0 +1,9 @@
+let base = prompt("Ingrese una base");
+let altura = prompt("Ingrese una altura");
+
+const calcularArea = (parametro1, parametro2) => {
+    area = parametro1*parametro2;
+    alert(area);
+}
+
+calcularArea(base, altura);
